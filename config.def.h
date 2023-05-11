68cfb6296d7e6381793c8f697f7eef53ed71ce6f
Sumit Modak <sumit.modak.solves@gmail.com>
2023-05-11 11:16:23 +0530

    default config changes

--- config.def.h
+++ config.def.h
@@ -5,13 +5,13 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "jetbrains-mono-fonts:size=16" };
+static const char dmenufont[]       = "jetbrains-mono-fonts:size=16";
 static const char nor_bg_col[]      = "#222222";
 static const char nor_border_col[]  = "#444444";
 static const char nor_fg_col[]      = "#bbbbbb";
 static const char sel_bg_col[]      = "#005577";
-static const char sel_border_col[]  = "#770000";
+static const char sel_border_col[]  = "#ddddff";
 static const char sel_fg_col[]      = "#eeeeee";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -40,7 +40,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[==]",     tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
@@ -58,8 +58,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", nor_bg_col, "-nf", nor_fg_col, "-sb", sel_bg_col, "-sf", sel_fg_col, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", nor_bg_col, "-nf", nor_fg_col, "-sb", sel_bg_col, "-sf", sel_fg_col, "-c", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        				function        argument */
@@ -68,33 +68,33 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_b,      				togglebar,      {0} },
 	{ MODKEY,                       XK_a,      				focusstack,     {.i = +1 } },						// shifts focus in clockwise manner
 	{ MODKEY,                       XK_s,      				focusstack,     {.i = -1 } },						// shifts focus in anticlockwise manner
-	{ MODKEY,                       XK_bracketleft,   incnmaster,     {.i = +1 } },						// brings one window 
-	{ MODKEY,                       XK_bracketright,  incnmaster,     {.i = -1 } },						// brings one window 
-	{ MODKEY|ShiftMask,             XK_a,      				setmfact,       {.f = -0.05} }, 					// resize window - makes left windows smaller
-	{ MODKEY|ShiftMask,             XK_s,      				setmfact,       {.f = +0.05} },					// resize window - makes right windows smaller
-	{ MODKEY,             					XK_Tab,    				zoom,           {0} },                   // brings window to the master 
-	{ MODKEY|ShiftMask,             XK_Tab,    				view,           {0} },										// moves to last used workspace
-	{ MODKEY,                       XK_q,      				killclient,     {0} },										// kill focused window
-	{ MODKEY,                       XK_t,      				setlayout,      {.v = &layouts[0]} },    // sets tiling layout mode
-	{ MODKEY,                       XK_f,      				setlayout,      {.v = &layouts[1]} },    // sets floating layout mode
-	{ MODKEY,                       XK_m,      				setlayout,      {.v = &layouts[2]} },    // sets monocle layout mode
-	{ MODKEY,                       XK_space,  				setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  				togglefloating, {0} },
+	{ MODKEY,                       XK_bracketleft,   incnmaster,     {.i = +1 } },						// brings one window on left side
+	{ MODKEY,                       XK_bracketright,  incnmaster,     {.i = -1 } },						// brings one window on right side
+	{ MODKEY,                       XK_k,      				setmfact,       {.f = -0.05} }, 				// resize window - makes left windows smaller
+	{ MODKEY,                       XK_v,      				setmfact,       {.f = +0.05} },					// resize window - makes right windows smaller
+	{ MODKEY,             					XK_Tab,    				zoom,           {0} },                  // brings window to the master 
+	{ MODKEY|ShiftMask,             XK_Tab,    				view,           {0} },									// moves to last used tag
+	{ MODKEY,                       XK_q,      				killclient,     {0} },									// kill focused window
+	{ MODKEY,                       XK_t,      				setlayout,      {.v = &layouts[0]} },   // sets tiling layout mode
+	{ MODKEY,                       XK_f,      				setlayout,      {.v = &layouts[1]} },   // sets floating layout mode
+	{ MODKEY,                       XK_m,      				setlayout,      {.v = &layouts[2]} },   // sets monocle layout mode
+	{ MODKEY,                       XK_space,  				setlayout,      {0} },									// toggle floating mode for current tag
+	{ MODKEY|ShiftMask,             XK_space,  				togglefloating, {0} },									// toggle floating mode for activated window
 	{ MODKEY,                       XK_0,      				view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      				tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  				focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, 				focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  				tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, 				tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,             				         0)
-	TAGKEYS(                        XK_2,                      				 1)
-	TAGKEYS(                        XK_3,  				                     2)
-	TAGKEYS(                        XK_4,          				             3)
-	TAGKEYS(                        XK_5,           				           4)
-	TAGKEYS(                        XK_6,                              5)
-	TAGKEYS(                        XK_7,                              6)
-	TAGKEYS(                        XK_8,                              7)
-	TAGKEYS(                        XK_9,                              8)
+	TAGKEYS(                        XK_1,             				        0)
+	TAGKEYS(                        XK_2,                      				1)
+	TAGKEYS(                        XK_3,  				                    2)
+	TAGKEYS(                        XK_4,          				            3)
+	TAGKEYS(                        XK_5,           				          4)
+	TAGKEYS(                        XK_6,                             5)
+	TAGKEYS(                        XK_7,                             6)
+	TAGKEYS(                        XK_8,                             7)
+	TAGKEYS(                        XK_9,                             8)
 	{ MODKEY|ShiftMask,             XK_q,             quit,           {0} },
 };
 
