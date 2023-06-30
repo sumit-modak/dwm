ec28b82f65454ac9ac6911836bb5e460c4cfddf1
Sumit Modak <sumit.modak.solves@gmail.com>
2023-06-30 18:58:03 +0530

    changed mouse configs

--- config.h
+++ config.h
@@ -125,14 +125,15 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} }, // toggles betwwen current and last selected layout
+	{ ClkLtSymbol,          0,              Button2,        setlayout,      {.v = &layouts[4]} },	// sets monocle layout on middle click
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[5]} },	// sets floating layout on right click
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
-	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },			// moves window using left click mouse
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
-	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },			// resizes window using right click mouse
+	{ ClkTagBar,            0,              Button1,        view,           {0} },			// views the tag pressed by left click mouse
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
